1c1dd2360ad2d8ddd9f204d076250d92e7899c91
kajetannow <kajetan_nowak@interia.eu>
2026-01-14 20:55:18 +0100

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https:/kajetannow.github.io',
+	site: 'http:/kajetannowak.pl',
 	integrations: [mdx(), sitemap()],
 	redirects: {
 		"/": "/photojournal"
