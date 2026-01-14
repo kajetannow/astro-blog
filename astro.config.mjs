c5bac7df4da37c8ef8d626742b366dff642e4eba
kajetannow <kajetan_nowak@interia.eu>
2026-01-14 21:03:37 +0100

    add domain (https)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http:/kajetannowak.pl',
+	site: 'https://kajetannowak.pl',
 	integrations: [mdx(), sitemap()],
 	redirects: {
 		"/": "/photojournal"
